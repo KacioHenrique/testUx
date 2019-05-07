add2790be543d0c42b604891e7b63db44c47c75e
Kacio Batista <kaciobatista@10-48-40-8.0101.ufpe.br.iosda.org>
2019-05-07 16:25:25 -0300

    primeira interacao pronta

--- ProjcReader/ViewController.swift
+++ ProjcReader/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet var text1: UITextView!
+    @IBOutlet var text2: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func blueAction(_ sender: Any) {
+        text1.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        text1.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+    @IBAction func redAction(_ sender: Any) {
+        text1.backgroundColor = #colorLiteral(red: 1, green: 0.4155011725, blue: 0.4733316118, alpha: 1)
+        text1.textColor = #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1)
+    }
 
-
+    @IBAction func swap(_ sender: Any) {
+    }
+    @IBAction func swipe(_ sender: UISwipeGestureRecognizer) {
+        if sender.direction == .right  || sender.direction == .left{
+            text1.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            text1.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            text2.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            text2.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        }
+    }
 }
 
